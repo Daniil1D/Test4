1c4903d9d86f201f146833e1186f06fd4072925a
Daniil Rakincev <rakincevdaniil18@gmail.com>
2023-06-08 18:26:46 +0500

    first commit

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
 <?php
-?>
-?>
-</body>
-</html>
-
+// размер файла
+echo filesize(filename: './one/t1.txt');
+//Создаем файл
+$fp=fopen(filename: './one/data.txt', mode: 'w');
+fwrite($fp,date('Y m d H:i:s', time()));
+fclose($fp);
